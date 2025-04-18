b1a12317f8b660543bf89bcb4a8f0e7d951fcb8a
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2025-04-18 14:32:57 +0800

    Refactor python script to make inputs more obvious

--- python/script.py
+++ python/script.py
@@ -3,20 +3,27 @@ from sys import argv
 from langchain.chat_models import init_chat_model
 from langchain_core.prompts import ChatPromptTemplate
 
+model = "llama3.2:1b"
+provider = "ollama"
+mode = argv[1]
+language = argv[2]
+query = argv[3]
+context = argv[4]
+
 system_prompt = {
     "ask": "You are a coding assistant that specializes in explaining code. When given a code snippet or a function, provide a clear, concise, and accurate explanation of what it does. Break down the logic, describe each part of the code, and explain the purpose of the overall function. If appropriate, also describe input/output behavior, edge cases, and real-world use. Always aim to make the explanation understandable to someone with basic programming knowledge.",
     "replace": "You are a coding assistant. Respond only with raw code — no explanations, no comments, no descriptions, no markdown formatting, and no code block tags. Never include justifications or instructions. Only output the exact code necessary to fulfill the request. NEVER include code tags or code fences.",
 }
 
-model = init_chat_model("llama3.2:1b", model_provider="ollama")
+model = init_chat_model(model, model_provider=provider)
 prompt = ChatPromptTemplate.from_messages([
-    ("system", system_prompt[argv[1]]),
+    ("system", system_prompt[mode]),
     ("system", "```{language}\n{context}```"),
     ("user", "{query}"),
 ])
 
 print((prompt | model).invoke({
-    "language": argv[2],
-    "query": argv[3],
-    "context": argv[4],
+    "language": language,
+    "query": query,
+    "context": context,
 }).content)
